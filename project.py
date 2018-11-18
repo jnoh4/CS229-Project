b89bf84c8732be1bbef1b2997290e11221242ab9
Joseph Noh <noh.j.joseph@gmail.com>
2018-11-18 11:35:35 -0800

    hello

--- project.py
+++ project.py
@@ -1,14 +1,57 @@
 import sys
 import numpy as np
-import pandas
+# import pandas
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+
+data_path = "C:/Users/Joseph/Google Drive/[Organized] Stanford/[18-19] Junior/Fall Quarter/CS 229/fMRI project/ICA_time_course-20181027T171839Z-001/ICA_time_course/"
+file_name = "NCANDA_S00033.txt"
+
+num_bins = 269
+
+def print_in_lines(arr, n):
+	size = len(arr)
+	for i in range(int(math.ceil(float(size) / n))):
+
+		if (n * i) + n <= size:
+			print(str(n*i) + " - " + str((n * i) + n - 1) + ":" + str(arr[(n*i) : (n*i) + n]))
+		else:
+			print(str(n*i) + " - " + str(size - 1) + ":" + str(arr[(n*i):]))
 
 def main():
-    f = np.loadtxt("NCANDA_S00033.txt")
-    print(f)
+	# Data introduction
+    f = np.loadtxt(data_path + file_name)
+    print("\nThe normalized data is organized as (time, region): \n")
     print(np.shape(f))
-    print(np.min(f))
-    print(np.max(f))
+    print("\n")
 
+    time_dur = len(f)
+    regions = len(f[1])
+
+    # Intial data processing
+
+    print("Here are the min max readings of each brain region: \n")
+
+    min_max = []
+    lin_dif = []
+    fold_change = []
+    for j in range(regions): 
+    	if j%2 == 1:
+    		plt.hist(f[:, j], num_bins)
+    	min_max.append([np.min(f[:, j]), np.max(f[:, j])])
+    	lin_dif.append(np.max(f[:, j]) - np.min(f[:, j]))
+    	# fold_change.append((np.max(f[:, j]) - np.min(f[:, j])) * 2 / (math.abs(np.max(f[:, j])) + math.abs(np.max(f[: , j]))))
+    	plt.show()
+    print(min_max)
+
+    print("\nHere are the linear differences: \n")
+    print_in_lines(lin_dif, 5)
+
+    # print("\nHere are the fold changes normalized to average of min and max values")
+    # print_in_lines(fold_change, 5)
 
 if __name__ == "__main__":
     main()
+
+
